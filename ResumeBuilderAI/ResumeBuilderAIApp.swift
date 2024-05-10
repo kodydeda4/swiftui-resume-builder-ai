42a5051cc1c85cf84e9233e07181991bbba917fe
Kody Deda <kodydeda4@gmail.com>
2024-05-10 10:12:57 -0400

    Initial Commit

---
+++ ResumeBuilderAI/ResumeBuilderAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeBuilderAIApp.swift
+//  ResumeBuilderAI
+//
+//  Created by Kody Deda on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ResumeBuilderAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
